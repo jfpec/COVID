ca9aaa98d7bee9d4e28f242d9fd93e530bf838dc
Francisco Pec <jfphernandez@gmail.com>
2021-12-09 17:53:24 -0600

    CAMBIO
    FINAL

--- app.R
+++ app.R
@@ -4,32 +4,30 @@ library(reshape2)
 library(dplyr)
 library(shinyWidgets)
 library(plotly)
-library(leaflet)
-library(sf)
+#library(leaflet)
+#library(sf)
 library(rgdal)
 library(spatialEco)
 library(shiny)
+library(tmap)
 
+data(World)
 consolidado <- read.csv("consolidado.csv")
-continente <- st_read("World_Continents.shp", quiet = T)
-continente <- sf::st_transform(continente, 4326)
 temp <- consolidado %>% group_by(ESTADO, PAIS, Lat, Long) %>% summarise(confirmed = max(confirmed, na.rm = T))
 temp <- data.frame(temp[,c("Lat","Long", "PAIS" ,"confirmed")])
 temp <- subset(temp, !c(is.na(Long) | Long == 0))
 coordinates(temp)= ~Long+Lat
 proj4string(temp) = CRS(sf::st_crs(4326)[[2]]) 
-pts <- data.frame(point.in.poly(temp, continente)@data)
+pts <- data.frame(point.in.poly(temp, World)@data)
 temp <- pts %>% group_by(PAIS) %>% summarise(confirmed = max(confirmed))
 pts2 <- inner_join(temp, pts, by = c("PAIS", "confirmed")) 
-pts2 <- pts2[,c("CONTINENT", "PAIS")]
+pts2 <- pts2[,c("continent", "PAIS")]
 consolidado <- full_join(consolidado, pts2, by = "PAIS")
-mun <- st_read("World_Countries__Generalized_.shp", quiet = T)
-mun <- sf::st_transform(mun, 4326)
-
+colnames(consolidado)[9]<-"CONTINENT"
+consolidado$CONTINENT <- as.character(consolidado$CONTINENT)
 
 ui <- navbarPage(title = "Dashboard COVID-19",
                 tabPanel("Temporal",
-                         
                          tabPanel("Comportamiento temporal",
                                   sidebarLayout(
                                     sidebarPanel(width = 3,
@@ -84,10 +82,11 @@ ui <- navbarPage(title = "Dashboard COVID-19",
                                       ),
                                       fluidRow(
                                         column(
-                                          width = 12, offset = 0,
-                                          leafletOutput("mapa_O1")
-                                        ) 
+                                          width = 11, offset = 0,
+                                          tmapOutput("mapa_O1")
+                                        ),
                                       ),
+                                      br(),
                                       fluidRow(
                                         column(
                                           width = 12, offset = 0,
@@ -98,19 +97,16 @@ ui <- navbarPage(title = "Dashboard COVID-19",
                                   )
                                   
                          )
-                         
-                         
-                         
                          ),
-                tabPanel("Resumen", "Boxplot para el porcentaje de casos recuperados de cada pais, agrupados por continente",
+                tabPanel("Resumen", 
                          fluidPage(
+                           p(strong("Boxplot para el porcentaje de casos recuperados de cada pais, agrupados por continente")),
                            fluidRow(
                              column(
                                width = 12, offset = 0,
                                plotlyOutput("plot02")
                                )
                              ),
-                           
                            p(strong("Boxplot para el porcentaje de mortalidad de cada pais, agrupados por continente")),
                            
                            fluidRow(
@@ -147,7 +143,8 @@ server <- function(input, output, session) {
                                                            recovered = sum(recovered, na.rm = T))
     table$FECHA <- as.Date(table$FECHA, format = "%m/%d/%y")
     if (length(input$variables)==1){
-      table <- table[,c("PAIS", "FECHA", "confirmed")]
+      
+      table <- table[,c("PAIS", "FECHA", input$variables)]
       colnames(table) <- c("PAIS", "FECHA", "X1")
       plot1 <- plot_ly(data=table, x = ~ FECHA, y = ~ X1, type = "scatter", mode="lines", color= ~ PAIS)
       plot1 <- plot1 %>% layout(title = "",
@@ -174,7 +171,8 @@ server <- function(input, output, session) {
           fplot1 <- plot1 %>% add_trace(y = ~ X3, mode = 'markers', name = paste0(input$variables[3], " cases")) 
       }else{}}}
   })
-  output$mapa_O1 <- renderLeaflet({
+
+  output$mapa_O1 <- renderTmap({
     table_f <- as.data.frame(res_mod())
     table_f$FECHA <- as.Date(table_f$FECHA, format = "%m/%d/%y")
     table_f <- table_f %>% subset(FECHA > input$date[1] & FECHA <input$date[2])
@@ -182,13 +180,13 @@ server <- function(input, output, session) {
                                                                          deaths = max(deaths),
                                                                          recovered = max(recovered))
     table <- data.frame(table1[,c("Lat","Long", "PAIS" ,"confirmed","deaths","recovered")])
-    table <- subset(table, !c(Long==0 | is.na(Long)))#na.omit()
+    table <- subset(table, !c(Long==0 | is.na(Long)))
     coordinates(table)= ~Long+Lat
     proj4string(table) = CRS(sf::st_crs(4326)[[2]])
-    ptsp <- data.frame(point.in.poly(table, mun)@data)
+    ptsp <- data.frame(point.in.poly(table, World)@data)
     temp <- ptsp %>% group_by(PAIS) %>% summarise(confirmed = max(confirmed, na.rm = T))
-    pts2 <- inner_join(temp, ptsp, by = c("PAIS", "confirmed")) 
-    pts2 <- na.omit(pts2[,c("ISO", "PAIS")])
+    pts2 <- inner_join(temp, ptsp, by = c("PAIS", "confirmed"))
+    pts2 <- na.omit(pts2[,c("iso_a3", "PAIS")])
 
     table2 <- table1 %>% 
       group_by(PAIS) %>% 
@@ -197,26 +195,12 @@ server <- function(input, output, session) {
                 recovered = sum(recovered, na.rm = T))
     pais <- inner_join(pts2, table2, by = "PAIS")
     
-    mun_estado<- merge(mun, pais, by.x ="ISO", by.y = "ISO" , all = FALSE)
-    
-    #table2 <- table2 %>% slice(order(factor(PAIS)))
-    mun_estado <- mun_estado %>%
-      filter(ISO %in% unique(ptsp$ISO))
-    
-    bins<-c(0, 1000, 10000, 100000, 500000, 1000000, 10000000,  100000000)
-    pal<-colorBin("YlOrRd", domain = table2$confirmed, bins = bins)
-    
-    leaflet(mun_estado) %>%
-      addTiles() %>%
-      addPolygons(fillColor = ~pal(confirmed),
-                  fillOpacity = 0.5,
-                  label = (sprintf("<strong>%s</strong><br/> Total confirmados %g</sup><br/> Total recuperados %g</sup><br/> Total fallecidos %g</sup>",
-                                   mun_estado$COUNTRY,
-                                   mun_estado$confirmed,
-                                   mun_estado$recovered,
-                                   mun_estado$deaths) %>% lapply(htmltools::HTML)),weight = 1) %>% 
-      addCircleMarkers(~table$Long, ~table$Lat, radius = ~table$confirmed/3000000, fillOpacity = 0.5, label = ~as.character(table$PAIS)) %>% 
-      addLegend(pal = pal, values = ~table2$confirmed, opacity = 0.7, title = "Total casos confirmados", position = "bottomright")
+    mun_estado<- merge(World, pais, by.x ="iso_a3", by.y = "iso_a3" , all = FALSE)
+    mun_estado$iso_a3 <-  NULL
+    mapa_O1 <- tm_shape(mun_estado, name = "name") + tm_polygons(col = input$variables[1], 
+                                                                 style = "cont", 
+                                                                 title = paste0("Total ", input$variables[1]))
+    mapa_O1
   })
   
   
@@ -237,6 +221,7 @@ server <- function(input, output, session) {
     colnames(table2) <- c("Pais", "Casos confirmados", "Total fallecidos", "Total recuperados", "Porcentaje de recuperados", "Porcentaje de mortalidad")
     table2
   })
+  
   output$plot02 <- renderPlotly({
     table_f <- consolidado
     table_f <- subset(table_f, !c(is.na(CONTINENT)|CONTINENT=="Australia"))
